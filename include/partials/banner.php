af7b4948fd288816a6efc5b4d230c861a1e2f418
Archives <markharvey@mark-harveys-computer-3.local>
2009-12-14 09:05:35 -0500

    Initial commit - core files imported from seekingmichigan.org-Wordpress

---
+++ include/partials/banner.php
@@ -0,0 +1,4 @@
+<style type="text/css" media="screen">
+#callout { background: url(http://seekingmichigan.org/images/scenes-<?= $scene; ?>-top.jpg) no-repeat 50% -0px; }
+#footer { background:  url(http://seekingmichigan.org/images/scenes-<?= $scene; ?>-bottom.jpg) no-repeat 50% 15%; }
+</style>
