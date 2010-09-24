878178c28435e88d90284d118a320ac5aa2dd2ea
Derek Kastner <dkastner@gmail.com>
2010-09-23 22:17:15 -0400

    allowing search within category

--- include/partials/search.php
+++ include/partials/search.php
@@ -2,6 +2,9 @@
   <form id="global-search" action="<?= $home_url; ?>" method="get" >
     <label for="search-top" class="hidden">Seek: </label>
     <input type="text" name="s" id="s" value=" " />
+    <? if($category): ?>
+      <input type="hidden" name="cat" value="<?= $category ?>" />
+    <? endif; ?>
     <label for="search-button" class="hidden">Search </label>
     <input type="submit" value=" " id="search-button" name="search-button" />
   </form>
